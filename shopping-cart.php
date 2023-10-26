01879097df0905c23ced4e8a685d421aae6ffa39
Carole Gérard <gerard.carole78@gmail.com>
2023-10-26 18:36:11 +0200

    end day

--- shopping-cart.php
+++ shopping-cart.php
@@ -1,23 +1,17 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-// Include necessary functions and components
-require "../AZ-store/layouts/header.php";
-require "../AZ-store/layouts/footer.php";
+    // Include necessary functions and components
+    require "../AZ-store/layouts/header.php";
+    require "../AZ-store/layouts/footer.php";
 
-session_start();
+    session_start();
+    // session_destroy();
 
-
-// Check if the shopping cart session variable exists
-if (!isset($_SESSION["shoppingCart"])) {
-    $_SESSION["shoppingCart"] = array();
-}
-
-$shoppingCart = $_SESSION["shoppingCart"];
-
-
-// Your shopping cart HTML and PHP code can go here
+    if (isset($_SESSION["shoppingCart"])) {
+        $shoppingCart = $_SESSION["shoppingCart"];
+    }
 ?>
 
 <!DOCTYPE html>
@@ -33,23 +27,31 @@ $shoppingCart = $_SESSION["shoppingCart"];
     <?php headerHtml(); ?>
 
     <!-- Your shopping cart content here -->
-    <div class="shopping-cart">
+    <div id="shopping-cart">
         <h2>Your Shopping Cart</h2>
         <?php
-        // Check if the shopping cart is not empty
-        if (!empty($shoppingCart)) {
-            foreach ($shoppingCart as $item) {
-				echo '<div class="cart-item">';
-				echo '<img src="' . $item['image_url'] . '" alt="' . $item['product'] . '">';
-				echo '<h3>' . $item['product'] . '</h3>';
-				echo '<p>' . $item['price'] . ' €</p>';
-				echo '<p>Quantity: ' . $item['quantity'] . '</p>';
-				echo '</div>';
-			}			
-        } else {
-            echo '<p>Your shopping cart is empty.</p>';
-        }
-        ?>
+            // Check if the shopping cart is not empty
+            if (!empty($shoppingCart)) {
+                foreach ($shoppingCart as $item) { ?>
+                    <div class="cart-item">
+                        <picture>
+                            <img src="<?php echo $item['image_url'] ?>" alt="<?php echo $item['product'] ?>">
+                        </picture>
+                        <h3><?php echo $item['product'] ?></h3>
+                        <p>Prix unitaire : <?php echo $item['price'] ?> €</p>
+                        <div class="btn-group">
+                            <button>-</button>
+                            <button><?php echo $item['quantity'] ?></button>
+                            <button>+</button>
+                        </div>
+                        <p>Total : <?php echo $item['quantity'] * $item['price'] ?> €</p>
+
+                    </div>
+            <?php }	
+            } else { ?>
+                <p>Your shopping cart is empty.</p>
+            <?php } ?>
+
     </div>
 
     <!-- Include the footer -->
