f3149c33632f80fbd38b024d900f53d9224ee184
Alexandre <vensalex1991@gmail.com>
2023-10-27 12:15:57 +0200

    add delete

--- shopping-cart.php
+++ shopping-cart.php
@@ -7,17 +7,27 @@ require "../AZ-store/layouts/header.php";
 require "../AZ-store/layouts/footer.php";
 
 session_start();
+// session_destroy();
 
 $totalOrder = 0;
 $totalOrderTVAC = 0;
 
-if (isset($_SESSION["shoppingCart"])) {
+if (isset($_SESSION["shoppingCart"]) && is_array($_SESSION["shoppingCart"])) {
     $shoppingCart = $_SESSION["shoppingCart"];
 
-    foreach ($shoppingCart as $product) {
-        $subtotal = $product['price'] * $product['quantity'];
-        $totalOrder += $subtotal;
+    if (isset($_POST['delete-item'])) {
+        $deleteItemId = $_POST['delete-item'];
+        if (isset($shoppingCart[$deleteItemId])) {
+            unset($shoppingCart[$deleteItemId]);
+        }
     }
+    foreach ($shoppingCart as $product) {
+        if (is_array($product)) { // Check if the item is an array
+            $subtotal = $product['price'] * $product['quantity'];
+            $totalOrder += $subtotal;
+        }
+    }
+
     $totalOrder = number_format($totalOrder, 2, '.', '');
     $tva = number_format($totalOrder * 0.21, 2, '.', '');
     $totalOrderTVAC = number_format($totalOrder * 1.21, 2, '.', '');
@@ -27,15 +37,11 @@ if (isset($_SESSION["shoppingCart"])) {
     $shoppingCart['totalOrderTVAC'] = $totalOrderTVAC;
 
     $_SESSION["shoppingCart"] = $shoppingCart;
+
+
 }
 
-/*if (isset($_POST['delete-item'])) {
-    $deleteItemId = $_POST['delete-item'];
-    if (isset($shoppingCart[$deleteItemId])) {
-        unset($shoppingCart[$deleteItemId]);
-        $_SESSION["shoppingCart"] = $shoppingCart;
-    }
-}*/
+
 ?>
 
 <!DOCTYPE html>
@@ -59,28 +65,26 @@ if (isset($_SESSION["shoppingCart"])) {
         foreach ($shoppingCart as $key => $item) {
             if ($key === 'totalOrder') {
                 break;
-            }
-            if (is_array($item)) {
-                ?>
-                <div class="cart-item">
-                    <picture>
-                        <img src="<?php echo $item['image_url'] ?>" alt="<?php echo $item['product'] ?>">
-                    </picture>
-                    <h3><?php echo $item['product'] ?></h3>
-                    <p><?php echo 'Total : '.$item['price'].'€'?> </p>
-                    <div class="btn-group">
-                        <button>-</button>
-                        <button><?php echo $item['quantity'] ?></button>
-                        <button>+</button>
-                    </div>
-                    <p><?php echo 'Total : '.$item['totalOrder'].'€' ?> </p>
-                    <form method="post" id="btn-delete">
-                        <input type="hidden" name="delete-item" value="<?php echo $item['id']; ?>">
-                        <button type="submit" class="delete-button">X</button>
-                    </form>
+            } ?>
+            <div class="cart-item">
+                <picture>
+                    <img src="<?php echo $item['image_url'] ?>" alt="<?php echo $item['product'] ?>">
+                </picture>
+                <h3><?php echo $item['product'] ?></h3>
+                <p>Prix unitaire : <?php echo $item['price'] ?> €</p>
+                <div class="btn-group">
+                    <button>-</button>
+                    <button><?php echo $item['quantity'] ?></button>
+                    <button>+</button>
                 </div>
-            <?php }
-        }
+                <form method="post" id="btn-delete">
+                    <input type="hidden" name="delete-item" value="<?php echo $item['id']; ?>">
+                    <button type="submit" class="delete-button">x</button>
+                </form>
+                <p>Total : <?php echo $item['quantity'] * $item['price'] ?> €</p>
+            </div>
+
+        <?php }
     } else { ?>
         <p>Your shopping cart is empty.</p>
     <?php } ?>
@@ -92,7 +96,6 @@ if (isset($_SESSION["shoppingCart"])) {
     <p><?php echo $shoppingCart['totalOrderTVAC'] ?></p>
 
 </div>
-
 <!-- Include the footer -->
 <?php footerHtml(); ?>
 </body>
