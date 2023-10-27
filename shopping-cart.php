357c3fe8b4f9867708e3979e1e0f0dde7c48f5e8
Carole Gérard <gerard.carole78@gmail.com>
2023-10-27 16:26:42 +0200

    scss button

--- shopping-cart.php
+++ shopping-cart.php
@@ -1,60 +1,61 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-// Include necessary functions and components
-require "../AZ-store/layouts/header.php";
-require "../AZ-store/layouts/footer.php";
+    // Include necessary functions and components
+    require "../AZ-store/layouts/header.php";
+    require "../AZ-store/layouts/footer.php";
 
-session_start();
-// session_destroy();
+    session_start();
+    // session_destroy();
 
-$totalOrder = 0;
-$totalOrderTVAC = 0;
+    $totalOrder = 0;
+    $totalOrderTVAC = 0;
 
-if (isset($_SESSION["shoppingCart"]) && is_array($_SESSION["shoppingCart"])) {
-$shoppingCart = $_SESSION["shoppingCart"];
+    if (isset($_SESSION["shoppingCart"]) && is_array($_SESSION["shoppingCart"])) {
+    $shoppingCart = $_SESSION["shoppingCart"];
 
-if (isset($_POST['delete-item'])) {
-    $deleteItemId = $_POST['delete-item'];
-    if (isset($shoppingCart[$deleteItemId])) {
-        unset($shoppingCart[$deleteItemId]);
+    if (isset($_POST['delete-item'])) {
+        $deleteItemId = $_POST['delete-item'];
+        if (isset($shoppingCart[$deleteItemId])) {
+            unset($shoppingCart[$deleteItemId]);
+        }
     }
-}
-if (isset($_POST['incr-item'])) {
-    $incrItemId = $_POST['incr-item'];
-    if (isset($shoppingCart[$incrItemId])) {
-        $shoppingCart[$incrItemId]['quantity']++;
-    }
-    $_SESSION["shoppingCart"] = $shoppingCart;
-}
-if (isset($_POST['decr-item'])) {
-    $decrItemId = $_POST['decr-item'];
-    if (isset($shoppingCart[$decrItemId])) {
-        if ($shoppingCart[$decrItemId]['quantity'] > 1) {
-            $shoppingCart[$decrItemId]['quantity']--;
-        } else {
-            // Si la quantité atteint 1, supprimez l'article du panier
-            unset($shoppingCart[$decrItemId]);
+    if (isset($_POST['incr-item'])) {
+        $incrItemId = $_POST['incr-item'];
+        if (isset($shoppingCart[$incrItemId])) {
+            $shoppingCart[$incrItemId]['quantity']++;
         }
         $_SESSION["shoppingCart"] = $shoppingCart;
     }
-}
-foreach ($shoppingCart as $product) {
-    if (is_array($product)) { // Check if the item is an array
-        $subtotal = $product['price'] * $product['quantity'];
-        $totalOrder += $subtotal;
+    if (isset($_POST['decr-item'])) {
+        $decrItemId = $_POST['decr-item'];
+        if (isset($shoppingCart[$decrItemId])) {
+            if ($shoppingCart[$decrItemId]['quantity'] > 1) {
+                $shoppingCart[$decrItemId]['quantity']--;
+            } else {
+                // Si la quantité atteint 1, supprimez l'article du panier
+                unset($shoppingCart[$decrItemId]);
+            }
+            $_SESSION["shoppingCart"] = $shoppingCart;
+        }
+    }
+    foreach ($shoppingCart as $product) {
+        if (is_array($product)) { // Check if the item is an array
+            $subtotal = $product['price'] * $product['quantity'];
+            $totalOrder += $subtotal;
 
 
-        $totalOrder = number_format($totalOrder, 2, '.', '');
-        $tva = number_format($totalOrder * 0.21, 2, '.', '');
-        $totalOrderTVAC = number_format($totalOrder * 1.21, 2, '.', '');
+            $totalOrder = number_format($totalOrder, 2, '.', '');
+            $tva = number_format($totalOrder * 0.21, 2, '.', '');
+            $totalOrderTVAC = number_format($totalOrder * 1.21, 2, '.', '');
 
-        $shoppingCart['totalOrder'] = $totalOrder;
-        $shoppingCart['tva'] = $tva;
-        $shoppingCart['totalOrderTVAC'] = $totalOrderTVAC;
+            $shoppingCart['totalOrder'] = $totalOrder;
+            $shoppingCart['tva'] = $tva;
+            $shoppingCart['totalOrderTVAC'] = $totalOrderTVAC;
 
-        $_SESSION["shoppingCart"] = $shoppingCart;
+            $_SESSION["shoppingCart"] = $shoppingCart;
+        }
     }
 }
 ?>
@@ -68,60 +69,60 @@ foreach ($shoppingCart as $product) {
     <title>Shopping Cart</title>
 </head>
 <body>
-<!-- Include the header -->
-<?php headerHtml(); ?>
+    <!-- Include the header -->
+    <?php headerHtml(); ?>
 
-<!-- Your shopping cart content here -->
-<main id="main-shopping-cart">
-    <div id="shopping-cart">
-        <h2>Your Shopping Cart</h2>
-        <?php
-        // Check if the shopping cart is not empty
-        if (!empty($shoppingCart) && is_array($shoppingCart)) {
-            foreach ($shoppingCart as $key => $item) {
-                if (is_array($item)) { ?>
-                    <div class="cart-item">
-                        <picture>
-                            <img src="<?php echo $item['image_url'] ?>" alt="<?php echo $item['product'] ?>">
-                        </picture>
-                        <h3><?php echo $item['product'] ?></h3>
-                        <p>Prix unitaire : <?php echo $item['price'] ?> €</p>
-                        <div class="btn-group">
-                            <form method="post" id="btn-decrement">
-                                <input type="hidden" name="decr-item" value="<?php echo $item['id']; ?>">
-                                <button type="submit" class="decr-button">-</button>
-                            </form>
-                            <button><?php echo $item['quantity'] ?></button>
-                            <form method="post" id="btn-increment">
-                                <input type="hidden" name="incr-item" value="<?php echo $item['id']; ?>">
-                                <button type="submit" class="incr-button">+</button>
-                            </form>
+    <!-- Your shopping cart content here -->
+    <main id="main-shopping-cart">
+        <div id="shopping-cart">
+            <h2>Your Shopping Cart</h2>
+            <?php
+            // Check if the shopping cart is not empty
+            if (!empty($shoppingCart) && is_array($shoppingCart)) {
+                foreach ($shoppingCart as $key => $item) {
+                    if (is_array($item)) { ?>
+                        <div class="cart-item">
+                            <picture>
+                                <img src="<?php echo $item['image_url'] ?>" alt="<?php echo $item['product'] ?>">
+                            </picture>
+                            <h3><?php echo $item['product'] ?></h3>
+                            <p>Prix unitaire : <?php echo $item['price'] ?> €</p>
+                            <div class="btn-group">
+                                <form method="post" id="btn-decrement">
+                                    <input type="hidden" name="decr-item" value="<?php echo $item['id']; ?>">
+                                    <button type="submit" class="decr-button">-</button>
+                                </form>
+                                <button><?php echo $item['quantity'] ?></button>
+                                <form method="post" id="btn-increment">
+                                    <input type="hidden" name="incr-item" value="<?php echo $item['id']; ?>">
+                                    <button type="submit" class="incr-button">+</button>
+                                </form>
 
+                            </div>
+                            <p>Total : <?php echo $item['quantity'] * $item['price'] ?> €</p>
+                            <form method="post" id="btn-delete">
+                                <input type="hidden" name="delete-item" value="<?php echo $item['id']; ?>">
+                                <button type="submit" class="btn-delete">x</button>
+                            </form>
                         </div>
-                        <p>Total : <?php echo $item['quantity'] * $item['price'] ?> €</p>
-                        <form method="post" id="btn-delete">
-                            <input type="hidden" name="delete-item" value="<?php echo $item['id']; ?>">
-                            <button type="submit" class="btn-delete">x</button>
-                        </form>
-                    </div>
-                <?php }
+                    <?php }
+                }
             }
-        }
-        } else { ?>
-            <p>Your shopping cart is empty.</p>
-        <?php } ?>
-    </div>
+            else { ?>
+                <p>Your shopping cart is empty.</p>
+            <?php } ?>
+        </div>
 
-    <div id="total-order">
-        <h3>Récapitulatif de la commande</h3>
-        <p>Total : <?php if (isset($shoppingCart['totalOrder'])) {
-                echo $shoppingCart['totalOrder'];
-            } ?> €</p>
-        <button><a href="checkout.php">Poursuivre la commande</a></button>
+        <div id="total-order">
+            <h3>Récapitulatif de la commande</h3>
+            <p>Total : <?php if (isset($shoppingCart['totalOrder'])) {
+                    echo $shoppingCart['totalOrder'];
+                } ?> €</p>
+            <button><a href="checkout.php">Poursuivre la commande</a></button>
 
-    </div>
-</main>
-<!-- Include the footer -->
-<?php footerHtml(); ?>
+        </div>
+    </main>
+    <!-- Include the footer -->
+    <?php footerHtml(); ?>
 </body>
 </html>
